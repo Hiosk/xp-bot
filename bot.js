5c4df8ce16176af57f3fb8f78f40a9bb9aa7d87e
leonardo.mendes <leonardo.mendes@e-xyon.com.br>
2022-12-01 23:37:52 -0300

    Adicao do dotenv

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const { Client, GatewayIntentBits, Partials } = require("discord.js");
 const { DisTube } = require("distube");
 const { SpotifyPlugin } = require("@distube/spotify");
